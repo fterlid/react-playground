9f23b9949f70d2a268d2f7ddbb37dabfb2a1318a
fterlid <fterlid@gmail.com>
2016-10-28 01:22:46 +0200

    Added react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
+const webpack = require('webpack');
+
 module.exports = {
-    entry: ['babel-polyfill', './src/client'],
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080/', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        'babel-polyfill',
+        './src/client'
+    ],
+
     output: {
         path: './dist',
-        publicPath: '/',
+        publicPath: 'http://localhost:8080/',
         filename: 'app.bundle.js'
     },
+
     resolve: {
         modulesDirectories: ['node_modules', 'src/shared'],
         extensions: ['', '.js', '.jsx']
     },
+
     module: {
         loaders: [{
             test: /\.jsx$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
         }]
-    }
+    },
+
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 };
