db552e626d363500efe8de6492622580697c9bf3
fterlid <fterlid@gmail.com>
2016-11-15 20:57:11 +0100

    async actions

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -6,12 +6,14 @@ import Routes from '../shared/routes';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import * as reducers from '../shared/reducers';
+import { applyMiddleware } from 'redux';
+import promiseMiddleware   from 'lib/promiseMiddleware';
 
 const app = Express();
 
 app.use((request, response) => {
     const reducer = combineReducers(reducers);
-    const store = createStore(reducer);
+    const store = applyMiddleware(promiseMiddleware)(createStore)(reducer);
 
     match({
         routes: Routes,
--- src/shared/actions/todoActions.js
+++ src/shared/actions/todoActions.js
@@ -1,9 +1,12 @@
+import request from 'axios';
+
+const BACKEND_URL = 'https://webtask.it.auth0.com/api/run/wt-milomord-gmail_com-0/redux-tutorial-backend?webtask_no_cache=1';
+
 export function createTodo(text) {
     return {
         type: 'CREATE_TODO',
-        text,
-        date: Date.now()
-    };
+        promise: request.post(BACKEND_URL, { text })
+    }
 }
 
 export function editTodo(id, text) {
